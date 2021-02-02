bf5641713843e1de317ca8564443dd73e861eb07
Ryan De Coster <ryandecoster@gmail.com>
2021-02-01 17:00:18 -0800

    implemented bubblesort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,27 +1,25 @@
-export{};
-
-bubbleSort(arr)=>{
-  let swapped;
+const bubbleSort = (arr) => {
+  let swapped = true;
   while (swapped) {
     swapped = false;
-    arr.forEach((current, i ) => {
-      if ( current > arr[i+1]) {
+    arr.forEach((current, i) => {
+      if (current > arr[i + 1]) {
         const temp = current;
-        current = arr[i+1]
-        arr[i+1] = temp
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
         swapped = true;
       }
-    })
+    });
   }
   return arr;
-}
+};
 
+const testArr1 = [2,1, 3,5, 6, 10, 9, 4, 7, 8];
 
-const testArr1 = [2,1,5,3,6,8,10,7,9]
+const testArr2 = [1, 2, 3, 5, 8, 9, 4, 6, 7, 10];
 
-const testArr2 = [2,1,5,3,6,8,9,7,10]
-console.log( bubbleSort(testArr1,testArr2))
+console.log(bubbleSort(testArr1), bubbleSort(testArr2));
 
-module.exports={
-  bubbleSort
+module.exports = {
+  bubbleSort,
 };
